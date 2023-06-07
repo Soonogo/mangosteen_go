c89de0b6180007ab38cd4f5321d4bc12058b1060
songen <614750803@qq.com>
2023-06-08 00:48:57 +0800

    Detachment generateDigists rand function

--- internal/controller/create_validation_code.go
+++ internal/controller/create_validation_code.go
@@ -10,6 +10,10 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Body struct {
+	Email string `json:"email" binding:"required,email"`
+}
+
 // validation godoc
 // @Summary      validation code
 // @Description  post validation code
@@ -19,27 +23,14 @@ import (
 // @Failure      500
 // @Router       /createValidationCode [post]
 func CreateValidationCode(c *gin.Context) {
-	var body struct {
-		Email string `json:"email" binding:"required,email"`
-	}
+	var body Body
 	if err := c.ShouldBindJSON(&body); err != nil {
 		c.String(400, "参数错误")
 		return
 	}
 	q := database.NewQuery()
 
-	len := 4
-	b := make([]byte, len)
-	_, err := rand.Read(b)
-	if err != nil {
-		log.Println("[CreateValidationCode]", err)
-	}
-	digits := make([]byte, len)
-	for i := range b {
-		log.Println(b[i])
-		digits[i] = b[i]%10 + 48
-	}
-	str := string(digits)
+	str := generateDigists(6)
 
 	log.Println("[CreateValidationCode]", body)
 	vc, err := q.CreateValidationCode(c, queries.CreateValidationCodeParams{
@@ -57,4 +48,18 @@ func CreateValidationCode(c *gin.Context) {
 		return
 	}
 	c.Status(200)
+}
+
+func generateDigists(len int) string {
+	b := make([]byte, len)
+	_, err := rand.Read(b)
+	if err != nil {
+		log.Println("[CreateValidationCode]", err)
+	}
+	digits := make([]byte, len)
+	for i := range b {
+		log.Println(b[i])
+		digits[i] = b[i]%10 + 48
+	}
+	return string(digits)
 }
