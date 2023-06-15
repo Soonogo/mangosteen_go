556a303394db064e659fd5076d6af92a279f096e
songen <614750803@qq.com>
2023-06-15 22:50:46 +0800

    move config environment

--- config/config.go
+++ config/config.go
@@ -9,7 +9,7 @@ import (
 func LoadAppConfig() {
 	viper.SetConfigName("config") // name of config file (without extension)
 	viper.SetConfigType("json")   // REQUIRED if the config file does not have the extension in the name
-	viper.AddConfigPath("$HOME/.mangosteen/")
+	viper.AddConfigPath("$HOME/.config/mangosteen/")
 	err := viper.ReadInConfig() // Find and read the config file
 	if err != nil {             // Handle errors reading the config file
 		panic(fmt.Errorf("fatal error config file: %w", err))
--- internal/jwt_helper/jwt_helper.go
+++ internal/jwt_helper/jwt_helper.go
@@ -3,15 +3,17 @@ package jwt_helper
 import (
 	"crypto/rand"
 	"io"
+	"io/ioutil"
 
 	"github.com/golang-jwt/jwt/v5"
+	"github.com/spf13/viper"
 )
 
 func GenerateJWT(user_id int) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"user_id": user_id,
 	})
-	key, err := generateHMACKey()
+	key, err := getHMACKey()
 
 	if err != nil {
 		return "", err
@@ -19,11 +21,16 @@ func GenerateJWT(user_id int) (string, error) {
 	return token.SignedString(key)
 }
 
-func generateHMACKey() ([]byte, error) {
+func GenerateHMACKey() ([]byte, error) {
 	key := make([]byte, 64)
 	_, err := io.ReadFull(rand.Reader, key)
 	if err != nil {
 		return nil, err
 	}
 	return key, nil
+}
+
+func getHMACKey() ([]byte, error) {
+	keyPath := viper.GetString("jwt.hmac.key_path")
+	return ioutil.ReadFile(keyPath)
 }
